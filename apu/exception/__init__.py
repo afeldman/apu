f019b0560518b47936354e920184b676f5d256ca
anton feldmann <anton.feldmann@aptiv.com>
2020-08-11 11:42:10 +0200

    blackboard

--- apu/exception/__init__.py
+++ apu/exception/__init__.py
@@ -1,4 +1,4 @@
-""" apu.dp: anton python utils design pattern module """
+""" apu.exception: anton python utils excaptions module """
 
 __version__ = (0, 0, 0)
 __email__ = "anton.feldmann@gmail.com"
