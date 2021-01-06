b63d0ac5f7567e57a0050f7270609c041c1f6ebf
anton feldmann <anton.feldmann@gmail.com>
2021-01-06 19:58:50 +0100

    gitutils

---
+++ apu/io/fileformat/np.py
@@ -0,0 +1,22 @@
+""" read numpy files """
+from numpy import load, save, savez
+from apu.io.fileformat import FileFormat
+
+class NPY(FileFormat):
+    """ handle npy files """
+    def read(self):
+        """ read npy files """
+        with open(self._filepath.absolute(), mode="br") as numpy_file:
+            self.data = load(numpy_file)
+
+    def write(self, sink:str, create:bool=False):
+        """ write npy files """
+        save(sink, self.data)
+        return self.data
+
+class NPZ(NPY):
+    """ handle npz files"""
+    def write(self, sink:str, create:bool=False):
+        """ write npz files """
+        savez(sink, self._args)
+        return self.data
