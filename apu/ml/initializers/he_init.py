2e4e78e95861c37abe019289a17df5deecd892fd
anton feldmann <anton.feldmann@gmail.com>
2021-01-06 17:39:03 +0100

    doc he initilizer

--- apu/ml/initializers/he_init.py
+++ apu/ml/initializers/he_init.py
@@ -1,15 +1,33 @@
 import math
 
-from torch.nn.init import _calculate_fan_in_and_fan_out,
+from torch.nn.init import (_calculate_fan_in_and_fan_out,
                           _no_grad_normal_,
                           _no_grad_uniform_,
                           xavier_uniform,
-                          xavier_normal
+                          xavier_normal)
 
 
 def he_uniform_(tensor, gain=1., mode="fan_in"):
     # type: (Tensor, float, str) -> Tensor
-    r"""
+    r"""Fills the input `Tensor` with values according to the method
+    described in `Delving Deep into Rectifiers: Surpassing Human-Level
+    Performance on ImageNet Classification' - Kaiming He, Xiangyu Zhang,
+    Shaoqing Ren, Jian Sun (2015), using a uniform
+    distribution. The resulting tensor will have values sampled from
+    :math:`\mathcal{U}(-a, a)` where
+
+    .. math::
+        a = \text{gain} \times \sqrt{\frac{2.0}{\text{fan}}}
+
+    Args:
+        tensor: an n-dimensional `torch.Tensor`
+        gain: an optional scaling factor
+        mode: an optional fan mode specifier
+
+    Examples:
+        >>> from apu.ml.initializers.he_init import he_uniform_
+        >>> w = torch.empty(3, 5)
+        >>> he_uniform_(w, gain=nn.init.calculate_gain('relu'), mode='fan_in')
     """
     fan_in, fan_out = _calculate_fan_in_and_fan_out(tensor)
 
@@ -25,7 +43,25 @@ def he_uniform_(tensor, gain=1., mode="fan_in"):
 
 def he_normal_(tensor, gain=1., mode="fan_in"):
     # type: (Tensor, float, str) -> Tensor
-    r"""
+    r"""Fills the input `Tensor` with values according to the method
+    described in `Delving Deep into Rectifiers: Surpassing Human-Level
+    Performance on ImageNet Classification' - Kaiming He, Xiangyu Zhang,
+    Shaoqing Ren, Jian Sun (2015), using a uniform
+    distribution.The resulting tensor will have values sampled from
+    :math:`\mathcal{N}(0, \text{std}^2)` where
+
+    .. math::
+        \text{std} = \text{gain} \times \sqrt{\frac{2.0}{\text{fan}}}
+
+    Args:
+        tensor: an n-dimensional `torch.Tensor`
+        gain: an optional scaling factor
+        mode: an optional fan mode specifier
+
+    Examples:
+        >>> from apu.ml.initializers.he_init import he_normal_
+        >>> w = torch.empty(3, 5)
+        >>> he_normal_(w, gain=nn.init.calculate_gain('relu'), mode='fan_in')
     """
     fan_in, fan_out = _calculate_fan_in_and_fan_out(tensor)
 
