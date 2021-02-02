5f12b4741dade62daf42f59454026c5bb57de33b
Feldmann <anton.feldmann@aptiv.com>
2021-02-02 16:34:51 +0100

    error in doc

--- apu/ml/init/__init__.py
+++ apu/ml/init/__init__.py
@@ -7,13 +7,9 @@ __author__ = "anton feldmann"
 
 from apu.ml.init.glorot import (glorot_normal, glorot_unified)
 from apu.ml.init.he import (he_normal,
-                            he_uniform,
-                            he_normal_,
-                            he_uniform_)
+                            he_uniform)
 
 __all__=['glorot_normal',
          "glorot_unified",
          "he_normal",
-         "he_normal_",
-         "he_uniform",
-         "he_uniform_"]
+         "he_uniform"]
--- apu/ml/init/glorot.py
+++ apu/ml/init/glorot.py
@@ -2,9 +2,9 @@
 it is more easy for me because tensorflow uses glorot
 """
 
-from torch.nn.init import (xavier_uniform,
-                          xavier_normal)
+from torch.nn.init import (xavier_uniform_,
+                          xavier_normal_)
 
 #simple aliasing
-glorot_normal = xavier_uniform
-glorot_unified = xavier_normal
+glorot_normal = xavier_uniform_
+glorot_unified = xavier_normal_
