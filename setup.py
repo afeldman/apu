ca8f534559fbba4cdde4ff0373f2a1d7c748083f
anton feldmann <anton.feldmann@gmail.com>
2021-04-21 16:16:51 +0200

    tqdm for copy

--- setup.py
+++ setup.py
@@ -11,14 +11,14 @@ requires_designpattern = ["dill"]
 requires_datetime = ["pytz", "pint", "tzlocal"]
 requires = ["GitPython"]
 requires_geographie = ["numpy"]
-requires_io = ["h5py","mat4py","pyyaml","dill","msgpack"]
+requires_io = ["h5py", "mat4py", "pyyaml", "dill", "msgpack"]
 if not platform.system().lower() == "windows":
     requires_io.append("python_magic")
 requires_all = (requires_datetime + requires + requires_geographie +
                 requires_designpattern + requires_io)
 
-
 import pathlib
+
 sys.path.append(pathlib.Path(__file__).parent.absolute())
 
 from apu import __version__
@@ -31,16 +31,16 @@ setup(
         'Source': 'https://github.com/afeldman/apu',
         'Tracker': 'https://github.com/afeldman/apu/issues',
     },
-    install_requires =[
+    install_requires=[
         "dill",
-        "pytz", 
-        "pint", 
+        "pytz",
+        "pint",
         "tzlocal",
         "GitPython",
         "numpy",
-        "mat4py"
+        "mat4py",
     ],
-    extra_requires ={
+    extra_requires={
         "all": requires_all,
         "datetime": requires_datetime,
         "setup": requires,
