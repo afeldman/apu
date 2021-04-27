140e20abe4e6f62c584ae303911c0002d170f765
anton feldmann <anton.feldmann@gmail.com>
2021-04-27 11:27:44 +0200

    small change

--- apu/io/copy.py
+++ apu/io/copy.py
@@ -1,12 +1,14 @@
 """ make recursive cpy easy"""
 import os
 import shutil
+import errno
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 from pathlib import Path
 from tqdm import tqdm
 
 from apu.io.fileformat import compair
+from apu.io.file_chunk import Chunk
 
 
 def copy_(file_, pbar=None):
@@ -69,6 +71,7 @@ class Copy:
                 print(f"{src_dir} is empty?")
                 continue
 
+            print("compare")
             with tqdm(total=len(file_list)) as pbar:
                 with ThreadPoolExecutor(max_workers=self.jobs) as ex:
                     futures = [
@@ -87,8 +90,6 @@ class Copy:
 
         if dst_file.exists():
             if not compair(src_file, dst_file, method="md5"):
-                print(f"{src_file} already copied")
-            else:
                 self.files.add(tuple((src_file, dst_file)))
         pbar.update(1)
 
@@ -97,6 +98,8 @@ class Copy:
         if self.files is None or len(self.files) == 0:
             return
 
+        print("start copy")
+
         with tqdm(total=len(self.files)) as pbar:
             with ThreadPoolExecutor(max_workers=self.jobs) as ex:
                 futures = [
@@ -104,3 +107,35 @@ class Copy:
                 ]
                 for future in as_completed(futures):
                     future.result()
+
+    @staticmethod
+    def large_file(src, dst):
+        """ copy a large file py chunks """
+        src = Path(src)
+        dst = Path(dst)
+
+        if not src.exists():
+            print(errno.ENOENT, f'ERROR: file does not exist: "{str(src)}"')
+            raise SystemExit()
+
+        if dst.exists():
+            os.remove(dst)
+
+        if dst.exists():
+            print(errno.EROFS,
+                  f' ERROR: file exists, cannot overwrite it: "{str(dst)}"')
+            raise SystemExit()
+
+        chunk_size = Chunk(src, divisor=10e5).size
+
+        try:
+            with open(src, 'rb') as ifp:
+                with open(dst, 'wb') as ofp:
+                    chunk = ifp.read(chunk_size)
+                    while chunk:
+                        ofp.write(chunk)
+                        chunk = ifp.read(chunk_size)
+
+        except IOError as ioerr:
+            print(f'ERROR: {ioerr}')
+            raise SystemExit() from ioerr
